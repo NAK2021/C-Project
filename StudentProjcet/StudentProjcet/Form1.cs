0c0937093f8974b4d8b852d590a39793e097408c
Julimorg <kienphongtran2003@gmail.com>
2023-11-29 23:55:47 +0700

    C

---
+++ StudentProjcet/StudentProjcet/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentProjcet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2TextBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+       
+
+        private void modaleffecttimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        public static int parentX, parentY;
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            Form modalBackground = new Form();
+            using (SignupForm modal = new SignupForm())
+            {
+                modalBackground.StartPosition = FormStartPosition.Manual;
+                modalBackground.FormBorderStyle = FormBorderStyle.None;
+                modalBackground.Opacity = .5d;
+                modalBackground.BackColor = Color.Black;
+                modalBackground.Size = this.Size;
+                modalBackground.Location = this.Location;
+                modalBackground.ShowInTaskbar = false;
+                modalBackground.Show();
+                modal.Owner = modalBackground;
+
+                parentX = this.Location.X;
+                parentY = this.Location.Y;
+
+                SignupForm m = new SignupForm();
+                m.ShowDialog();
+
+                modalBackground.Dispose();  
+
+            }
+        }
+    }
+}
